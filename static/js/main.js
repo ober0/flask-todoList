9fa549c33204324efff59f01c41fc14b4416f949
Ruslan <onishruslan@yandex.ru>
2024-06-09 15:46:11 +0300

    Доработано добавление задач

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,6 @@
 function newElement() {
     let textInput = document.getElementById('myInput')
     let text = textInput.value
-
     fetch('/newtask', {
         method: 'post',
         headers: {
@@ -12,10 +11,28 @@ function newElement() {
         .then(response => response.json())
         .then(result => {
             if (result.success){
+                let taskCountList = document.querySelectorAll('.close')
+                let tclLength = taskCountList.length
+                if (tclLength == 0){
+                    window.location.reload()
+                }
+                let taskCount = taskCountList[tclLength-1].getAttribute('for')
+
+
+
                 const parent = document.getElementById('myUL')
 
                 const child = document.createElement('li')
                 child.innerText = text
+                child.classList.add('task')
+
+                var span = document.createElement("SPAN");
+                var txt = document.createTextNode("\u00D7");
+                span.className = "close";
+                span.appendChild(txt);
+
+                span.setAttribute('for', parseInt(taskCount)+1)
+                child.appendChild(span);
 
                 parent.appendChild(child)
             }
@@ -25,3 +42,5 @@ function newElement() {
 
         })
 }
+
+
